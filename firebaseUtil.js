629d4d4c03eb8d20389fdc42bfe53c48f587d2f3
young <youngblue-zero@daum.net>
2017-09-05 00:38:43 +0900

    [회사] 자동 데이터 입력은 회사 상세 -> 회사 전체로 이동.  회사 키를 얻기 쉬운 장점이 있다.

--- firebaseUtil.js
+++ firebaseUtil.js
@@ -9,14 +9,15 @@ function FirebaseUtil(){
         });
     };
     
-    this.moveFbRecord = function(oldRef, newRef) {    
-         oldRef.once('value', function(snap)  {
-          newRef.set( snap.val(), function(error) {
-               if( !error ) { oldRef.remove(); }
-               else if( typeof(console) !== 'undefined' && console.error ) {  console.error(error); }
-          });
-        });
-    };
+    // memo : ref가 하나의 덩어리가 아닌 여러개의 스냅샷이라면 remove가 동작하지 않음. copy 후 remove를 사용.
+//    this.moveFbRecord = function(oldRef, newRef) {    
+//         oldRef.once('value', function(snap)  {
+//          newRef.set( snap.val(), function(error) {
+//               if( !error ) { oldRef.remove(); }
+//               else if( typeof(console) !== 'undefined' && console.error ) {  console.error(error); }
+//          });
+//        });
+//    };
     
     this.removeFbRecord = function(inputDB, ref){
         ref.on('value', function(snapshot){
